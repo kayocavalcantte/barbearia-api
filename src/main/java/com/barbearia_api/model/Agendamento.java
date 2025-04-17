4c8584bd47b371ad175e8d9003980e91803887cb
kayocavalcantte <kayo.duarte@prolins.com.br>
2025-04-17 19:50:15 -0300

    first commit

---
+++ src/main/java/com/barbearia_api/model/Agendamento.java
@@ -0,0 +1,96 @@
+package com.barbearia_api.model;
+
+import jakarta.persistence.*;
+
+import java.sql.Date;
+import java.sql.Time;
+
+@Entity
+@Table(name = "agendamentos")
+public class Agendamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "funcionario_id", nullable = false)
+    private Funcionario funcionario;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    @Column(nullable = false)
+    private Time horario;
+
+    @Column(name = "data_agendamento", nullable = false)
+    private Date dataAgendamento;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status_agendamento", nullable = false)
+    private StatusAgendamento statusAgendamento = StatusAgendamento.espera;
+
+    @Column(nullable = false, unique = true)
+    private String crc;
+
+    public enum StatusAgendamento {
+        atendido, desmarcado, espera
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Time getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Time horario) {
+        this.horario = horario;
+    }
+
+    public Date getDataAgendamento() {
+        return dataAgendamento;
+    }
+
+    public void setDataAgendamento(Date dataAgendamento) {
+        this.dataAgendamento = dataAgendamento;
+    }
+
+    public StatusAgendamento getStatusAgendamento() {
+        return statusAgendamento;
+    }
+
+    public void setStatusAgendamento(StatusAgendamento statusAgendamento) {
+        this.statusAgendamento = statusAgendamento;
+    }
+
+    public String getCrc() {
+        return crc;
+    }
+
+    public void setCrc(String crc) {
+        this.crc = crc;
+    }
+}
---
+++ src/main/java/com/barbearia_api/model/Funcionario.java
@@ -0,0 +1,49 @@
+package com.barbearia_api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "funcionarios")
+public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(name = "is_ativo", nullable = false)
+    private boolean isAtivo;
+
+    public Funcionario() {}
+
+    public Funcionario(String nome, boolean isAtivo) {
+        this.nome = nome;
+        this.isAtivo = isAtivo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isAtivo() {
+        return isAtivo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.isAtivo = ativo;
+    }
+}
